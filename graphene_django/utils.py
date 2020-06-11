5e64a4f9a5c5b500a7d705712af76c4620d9da93
ariel1899 <radke1899@gmail.com>
2020-06-11 15:30:45 -0400

    Removing prints

--- graphene_django/utils.py
+++ graphene_django/utils.py
@@ -141,12 +141,6 @@ def auth_resolver(parent_resolver, permissions, attname, default_value, raise_ex
 
     user_permissions = args.pop("user_permissions", lambda _: True)
 
-    print(permissions)
-    print(user.email)
-    print(list(user.user_permissions.all()))
-    print(has_permissions(user, permissions))
-    print(user_permissions(user))
-
     if has_permissions(user, permissions) and user_permissions(user):
         if parent_resolver:
             # A resolver is provided in the class
